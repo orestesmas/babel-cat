e2794dcab9c7d1de3c376ec557b5b02a73d5c06e
Orestes Mas <orestes@tsc.upc.edu>
2021-06-25 21:13:31 +0200

    Added CTAN upload info to build.lua

--- build.lua
+++ build.lua
@@ -10,3 +10,15 @@ tdsroot      = "generic"
 
 checkruns    = 2
 checkconfigs = {"check-configs/latex", "check-configs/babel", "check-configs/plain"}
+
+uploadconfig = {
+  pkg          = "babel-catalan",
+  version      = "v3.0",
+  author       = "Miquel Ortega; Orestes Mas",
+  uploader     = "Miquel Ortega"
+  license      = "lppl1.3c",
+  summary      = "Contributed language-support files for Babel",
+  ctanPath     = "/macros/latex/contrib/babel-contrib/catalan",
+  repository   = "https://github.com/orestesmas/babel-cat",
+  note         = [[Uploaded automatically by l3build...]]
+}
